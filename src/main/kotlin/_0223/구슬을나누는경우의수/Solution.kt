7529a4c6bdbbe6ebddeee69a55ac63813ffeb374
jinlee0 <wls4370@gmail.com>
2023-02-23 23:36:01 +0900

    optimization: move map operation into fold operation

--- src/main/kotlin/_0223/구슬을나누는경우의수/Solution.kt
+++ src/main/kotlin/_0223/구슬을나누는경우의수/Solution.kt
@@ -5,7 +5,5 @@ import kotlin.math.min
 class Solution {
     fun solution(balls: Int, share: Int) =
         (1..min(share, (balls - share)))
-            .map { it to balls - it + 1 }
-            .fold(1.0) {acc, pair -> acc * pair.second / pair.first }
-
+            .fold(1.0) {acc, i -> acc * (balls - i + 1) / i }
 }
