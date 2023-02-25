de86aec6cc603f0465331833b38c67de2366bb8d
jinlee0 <wls4370@gmail.com>
2023-02-25 15:16:08 +0900

    add alternative solution using tailrec

--- src/main/kotlin/_0223/팩토리얼/Solution.kt
+++ src/main/kotlin/_0223/팩토리얼/Solution.kt
@@ -2,12 +2,15 @@ package _0223.팩토리얼
 
 class Solution {
     fun solution(n: Int) = generateSequence(1) { it+1 }
-        .first { it.factorial() > n } - 1
+        .first { it.factorial() > n }.dec()
 }
 
 private fun Int.factorial(): Int {
     return (2..this).fold(1) {acc, i -> acc * i }
 }
+
+/* 재귀
+ private tailrec fun factorial(n: Int, run: Int = 1): Int = if (n == 1) run else factorial(n - 1, run * n)*/
 
 fun main() {
     arrayOf(
