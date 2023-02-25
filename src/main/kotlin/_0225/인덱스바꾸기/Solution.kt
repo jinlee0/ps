4f6499c0ed7ff385ef559976ac5e82bde13c468b
jinlee0 <wls4370@gmail.com>
2023-02-25 17:29:51 +0900

    refactor String.swapped simple

--- src/main/kotlin/_0225/인덱스바꾸기/Solution.kt
+++ src/main/kotlin/_0225/인덱스바꾸기/Solution.kt
@@ -1,17 +1,27 @@
 package _0225.인덱스바꾸기
 
+import java.lang.StringBuilder
+
 
 class Solution {
     fun solution(my_string: String, num1: Int, num2: Int) = my_string.swapped(num1, num2)
 }
 
+//private fun String.swapped(index1: Int, index2: Int): String {
+//    return this.toCharArray().let {arr ->
+//        val temp = arr[index1]
+//        arr[index1] = arr[index2]
+//        arr[index2] = temp
+//        arr.fold(StringBuilder()){sb, c -> sb.append(c) }.toString()
+//    }
+//}
+
+// 이거 쿨하다
 private fun String.swapped(index1: Int, index2: Int): String {
-    return this.toCharArray().let {arr ->
-        val temp = arr[index1]
-        arr[index1] = arr[index2]
-        arr[index2] = temp
-        arr.fold(StringBuilder()){sb, c -> sb.append(c) }.toString()
-    }
+    return StringBuilder().also {
+        it.setCharAt(index1, this[index1])
+        it.setCharAt(index2, this[index2])
+    }.toString()
 }
 
 //private fun CharArray.foldToString(operation: (c: Any) -> Any): String {
