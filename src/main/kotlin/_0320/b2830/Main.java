795106f1b3e7d7e2e62ec254efb83699d6514c66
jinlee0 <wls4370@gmail.com>
2023-03-20 15:12:03 +0900

    2023-03-20

--- src/main/kotlin/_0320/b2830/Main.java
+++ src/main/kotlin/_0320/b2830/Main.java
@@ -3,29 +3,23 @@ package _0320.b2830;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
-        int[] results = new int[20]; // name은 2진수로 최대 20자리
+        int[] ones = new int[20]; // name은 2진수로 최대 20자리
         for (int i = 0; i < n; i++) {
             int name = Integer.parseInt(br.readLine());
-            int x = 0;
-            while(true) {
-                if(name >> 1 == 0) {
-                    results[x]++;
-                    break;
-                }
-                results[x++] += name - ((name >> 1) << 1);
-                name >>= 1;
+            for(int j = 0; j < 20; j++) {
+                if((name & (1 << j)) > 0) ones[j]++;
             }
         }
 
-        int sum = 0;
+        long sum = 0L;
         for (int i = 0; i < 20; i++) {
-            sum += results[i] * (n - results[i]) * (1 << i);
+            // 0의 개수 * 1의 개수 * 2^자리수
+            sum += (1L << i) * ones[i] * (n - ones[i]);
         }
         System.out.println(sum);
     }
